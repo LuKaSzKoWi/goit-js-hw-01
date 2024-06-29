da8bbafb5ce1b7860d9d7787a890c6496df5d357
LuKaSzKoWi <l.kowalski8509@gmail.com>
2024-06-29 17:41:14 +0200

    Add js code to index.html

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@ function makeTransaction(quantity, pricePerDroid) {
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
-
+// Check result
 console.log(makeTransaction(5, 3000)); 
 console.log(makeTransaction(3, 1000)); 
 console.log(makeTransaction(10, 500)); 
